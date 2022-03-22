35ccf94c451f8d80404fe871843c2747e3fdd3d9
Ronald Marques <ronaldpmarques01@gmail.com>
2022-03-22 19:51:40 -0300

    gofinances - fonts

--- gofinances/src/screens/Dashboard/index.tsx
+++ gofinances/src/screens/Dashboard/index.tsx
@@ -1,11 +1,32 @@
 import React from "react";
-import { Container, Title } from "./styles";
+
+import { Text, View } from "react-native";
+
+import { 
+    Container, 
+    Header,
+    UserWrapper,
+    ProfileBox,
+    Photo,
+    User,
+    UserName,
+    UserGreetings
+ } from "./styles";
 
 export function Dashboard(){
     return(
         <Container>
-            <Title>Dashboard.</Title>
+            <Header>
+                <UserWrapper>
+                    <ProfileBox>
+                        <Photo source={{ uri: "https://avatars.githubusercontent.com/u/90390537?v=4" }}></Photo>
+                        <User>
+                            <UserGreetings>Ola,</UserGreetings>
+                            <UserName>Ronald</UserName>
+                        </User>
+                    </ProfileBox>
+                </UserWrapper>
+            </Header>
         </Container>
-        
     )
 }
--- gofinances/src/screens/Dashboard/styles.ts
+++ gofinances/src/screens/Dashboard/styles.ts
@@ -1,14 +1,50 @@
 import styled from "styled-components/native";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
     flex: 1;
-    align-items: center;
-    justify-content: center;
-    background: ${({ theme }) => theme.colors.primary};
+    background: ${({ theme }) => theme.colors.background};
 `
 
-export const Title = styled.Text`
-    font-size: 30px;
-    font-weight: bold;
-    color: ${({ theme }) => theme.colors.secondary};
+export const Header = styled.View`
+    width: 100%;
+    height: ${RFPercentage(42)}px;
+
+    align-items: center;
+    justify-content: center;
+
+    background-color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const UserWrapper = styled.View`
+    width: 100%;
+
+    padding: 0 ${RFValue(24)}px;
+`;
+
+export const ProfileBox = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const Photo = styled.Image`  
+    width: ${RFValue(48)}px;
+    height: ${RFValue(48)}px;
+
+    border-radius: 10px;
+`;
+
+export const User = styled.View`
+    margin-left: 17px;
 `
+
+export const UserName = styled.Text`
+    color: ${({ theme }) => theme.colors.shape};
+    font-family: ${({ theme }) => theme.fonts.bold};
+    font-size: ${RFValue(18)}px;
+`
+export const UserGreetings = styled.Text`
+    color: ${({ theme }) => theme.colors.shape};
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size: ${RFValue(18)}px;
+`;
